81d0e8414f86d2a363d3d247fb58ab87909edf97
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 21:28:46 +0600

    Add project files.

---
+++ Mod_2_7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mod_2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Прошло 224 дня");
+            int days = 224;
+            int week = days / 7;
+            Console.WriteLine("Полных недель прошло: " + week);
+        }
+    }
+}
